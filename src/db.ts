ff8ca0aa26c1b23a0c2a82c69fbfb41cfb85d4b1
andrewlfauth <andrewlfauth@gmail.com>
2022-09-18 13:53:17 -0700

    connect to MongoDB

---
+++ src/db.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+let db
+connect()
+
+async function connect() {
+  if (db) return db;
+
+  if (process.env.NODE_ENV === "production") {
+    db = await mongoose.connect(import.meta.env.MONGODB_URL, {
+      useNewUrlParser: true,
+    })
+      .catch((err) => console.error(err))
+  } else {
+    if (!global.__db) {
+      global.__db = await mongoose.connect(import.meta.env.MONGODB_URL, {
+        useNewUrlParser: true,
+      })
+        .catch((err) => console.error(err))
+    }
+    db = global.__db;
+  }
+  return db;
+}
+
+export { mongoose, connect };
