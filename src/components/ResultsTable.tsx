859a80fa72e156e4fc620363bf6547858f792650
andrewlfauth <andrewlfauth@gmail.com>
2022-09-19 00:40:35 -0700

    get percentages from wins and losses

--- src/components/ResultsTable.tsx
+++ src/components/ResultsTable.tsx
@@ -2,36 +2,60 @@ import type {FunctionComponent} from 'preact'
 
 const getImageForDog = (dog: string) => `https://images.dog.ceo/breeds/dachshund/${dog}`
 
+
 const ResultsTable: FunctionComponent<{
-    totalVotes: number, 
-    results: object
-  }> = ({totalVotes, results}) => {
-  // let results = {'Dachshund_rabbit.jpg': 12, 'Daschund-2.jpg': 57}
-  const entries = Object.entries(results).sort((a, b) => b[1] - a[1])
+  totalVotes: number, 
+  candidates: object
+}> = ({totalVotes, candidates}) => {
+  const entries = Object.entries(candidates) 
+  let results = []
 
-  console.log(entries)
-
+  const getPercentage = (candidate: [
+    string, 
+    { wins: number; losses: number; }
+  ]) => {
+    let dog = candidate[0]
+    let wins = candidate[1].wins
+    let losses = candidate[1].losses
+    
+    if (wins + losses === 0) {
+      return results.push([dog, 0])
+    } 
+    return results.push([
+        dog, 
+        ((wins / (wins + losses)) * 100).toFixed(2)
+      ])
+  }
+  entries.forEach(e => getPercentage(e))
+  const sorted = results.sort((a, b) => b[1] - a[1])
+  
   return (
     <div class="flex justify-between flex-col lg:flex-row">
       <div>
         <h2 class="text-4xl pb-6">Winning Now</h2>
         <img 
-          src={getImageForDog(entries[0][0])} 
+          src={getImageForDog(sorted[0][0])} 
           alt="least cute dog"
           class="max-w-[500px] max-h-[500px] rounded-lg" 
         />
       </div>
-      <div class="grid grid-cols-2 gap-4 mt-4 lg:mt-0">
-        {entries.map((k) => (
-          <div class="flex items-center px-4 rounded-lg py-2 bg-slate-700">
-            <img 
-              src={getImageForDog(k[0])} 
-              alt="dog"
-              class="h-20 rounded-lg" 
-            />
-            <p class="text-2xl ml-2 font-semibold">{`${(k[1] / totalVotes).toFixed(2).split("0.").join("")}%`}</p>
-          </div>
-        ))}
+      <div>
+        <span>Total Votes: {totalVotes}</span>
+        <div class="grid lg:grid-cols-2 gap-4 mt-4 lg:mt-0 mx-10">
+          {sorted.map((k, i) => (
+            <div class="flex items-center pl-8 rounded-lg py-2 bg-slate-700 relative">
+              <div class="rounded-tl-lg rounded-br-lg absolute top-0 left-0 bg-slate-800 px-2 font-semibold text-lg">
+                {i + 1}
+              </div>
+              <img 
+                src={getImageForDog(k[0])} 
+                alt="dog"
+                class="h-20 rounded-lg hover:scale-[3] duration-300 hover:z-10" 
+              />
+              <p class="text-2xl ml-2 font-semibold">{k[1]}%</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
