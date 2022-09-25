2d8a0c039eb16e906d18178d5002cd72af2035dc
andrewlfauth <andrewlfauth@gmail.com>
2022-09-25 13:36:58 -0700

    Added: useImageCache hook

--- src/components/DogListing.tsx
+++ src/components/DogListing.tsx
@@ -1,12 +1,14 @@
-import type {FunctionComponent} from 'preact'
-import {useState} from 'preact/hooks'
+import type { FunctionComponent } from 'preact'
+import { useState } from 'preact/hooks'
 import { getVotingOptions } from '../data/getRandom'
+import useImageCache from './hooks/useImageCache'
 
-const getImageForDog = (dog: string) => `https://images.dog.ceo/breeds/dachshund/${dog}`
+const getImageForDog = (dog: string) =>
+  `https://images.dog.ceo/breeds/dachshund/${dog}`
 
-const DogListing: FunctionComponent<{a: string, b: string}> = ({a, b}) => {
+const DogListing: FunctionComponent<{ a: string; b: string }> = ({ a, b }) => {
   const [loading, setLoading] = useState(false)
-  const [current, setcurrent] = useState([a,b])
+  const [current, setcurrent] = useState([a, b])
   const [next, setNext] = useState(getVotingOptions())
   const handleA = async () => {
     setLoading(true)
@@ -16,8 +18,8 @@ const DogListing: FunctionComponent<{a: string, b: string}> = ({a, b}) => {
     }
     setcurrent([next[0], next[1]])
     await fetch('/api/vote', {
-      method: "post",
-      body: JSON.stringify(result)
+      method: 'post',
+      body: JSON.stringify(result),
     })
     setNext(getVotingOptions())
     setLoading(false)
@@ -30,40 +32,48 @@ const DogListing: FunctionComponent<{a: string, b: string}> = ({a, b}) => {
     }
     setcurrent([next[0], next[1]])
     await fetch('/api/vote', {
-      method: "post",
-      body: JSON.stringify(result)
+      method: 'post',
+      body: JSON.stringify(result),
     })
     setNext(getVotingOptions())
     setLoading(false)
   }
 
+  useImageCache()
+
   return (
-    <div class="flex flex-col md:flex-row text-gray-100 justify-between items-center">
-      <div class="flex flex-col items-center justify-center space-y-4">
-        <img class="h-44 rounded-lg" src={getImageForDog(current[0])} alt="dog1" />
-        <button 
+    <div class='flex flex-col md:flex-row text-gray-100 justify-between items-center'>
+      <div class='flex flex-col items-center justify-center space-y-4'>
+        <img
+          class='h-44 rounded-lg'
+          src={getImageForDog(current[0])}
+          alt='dog1'
+        />
+        <button
           onClick={handleA}
           disabled={loading}
-          class="bg-slate-800 hover:bg-slate-700 duration-200 px-4 py-2 rounded text-xl"
+          class='bg-slate-800 hover:bg-slate-700 duration-200 px-4 py-2 rounded text-xl'
         >
           This one is
         </button>
       </div>
-      <span class="md:mx-10 md:my-0 my-10 text-lg font-semibold">
-        vs
-      </span>
-      <div class="flex flex-col items-center justify-center space-y-4">
-        <img class="h-44 rounded-lg" src={getImageForDog(current[1])} alt="dog1" />
+      <span class='md:mx-10 md:my-0 my-10 text-lg font-semibold'>vs</span>
+      <div class='flex flex-col items-center justify-center space-y-4'>
+        <img
+          class='h-44 rounded-lg'
+          src={getImageForDog(current[1])}
+          alt='dog1'
+        />
         <button
           onClick={handleB}
-          disabled={loading}  
-          class="bg-slate-800 px-4 py-2 hover:bg-slate-700 duration-200 rounded text-xl"
+          disabled={loading}
+          class='bg-slate-800 px-4 py-2 hover:bg-slate-700 duration-200 rounded text-xl'
         >
           This one is
         </button>
       </div>
     </div>
-  ) 
+  )
 }
 
 export default DogListing
